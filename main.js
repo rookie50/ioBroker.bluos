0790c29d047139c67233f2bbfee6dea72c8877a0
rookie50 <57443640+rookie50@users.noreply.github.com>
2023-07-19 09:59:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,22 +11,54 @@ const utils = require("@iobroker/adapter-core");
 // Load your modules here, e.g.:
 // const fs = require("fs");
 
-class Bluos extends utils.Adapter {
 
-	/**
-	 * @param {Partial<utils.AdapterOptions>} [options={}]
-	 */
-	constructor(options) {
-		super({
-			...options,
-			name: "bluos",
-		});
-		this.on("ready", this.onReady.bind(this));
-		this.on("stateChange", this.onStateChange.bind(this));
-		// this.on("objectChange", this.onObjectChange.bind(this));
-		// this.on("message", this.onMessage.bind(this));
-		this.on("unload", this.onUnload.bind(this));
-	}
+class BluOSAdapter extends utils.Adapter {
+  constructor(options) {
+    super({
+      ...options,
+      name: 'bluos-adapter',
+    });
+    this.devicesDir = `${this.namespace}.BluOS.Devices`;
+    this.groupsDir = `${this.namespace}.BluOS.Groups`;
+    this.devicesConfig = {
+      type: 'array',
+      title: 'Devices',
+      items: {
+        type: 'object',
+        properties: {
+          name: {
+            type: 'string',
+            title: 'Name'
+          },
+          ip: {
+            type: 'string',
+            title: 'IP Address'
+          }
+        }
+      }
+    };
+    this.groupsConfig = {
+      type: 'array',
+      title: 'Groups',
+      items: {
+        type: 'object',
+        properties: {
+          name: {
+            type: 'string',
+            title: 'Name'
+          },
+          devices: {
+            type: 'array',
+            title: 'Devices',
+            items: {
+              type: 'string'
+            }
+          }
+        }
+      }
+    };
+    this.devices = [];
+  }
 
 	/**
 	 * Is called when databases are connected and adapter received configuration.
@@ -87,7 +119,197 @@ class Bluos extends utils.Adapter {
 		result = await this.checkGroupAsync("admin", "admin");
 		this.log.info("check group user admin group admin: " + result);
 	}
+    // Get devices configuration
+    const devicesConfig = await this.getForeignObjectAsync(this.devicesDir);
+    if (devicesConfig && devicesConfig.common && devicesConfig.common.default) {
+      this.devices = JSON.parse(devicesConfig.common.default);
+    }
 
+    // Get groups configuration
+    const groupsConfig = await this.getForeignObjectAsync(this.groupsDir);
+    if (!groupsConfig || !groupsConfig.common || !groupsConfig.common.default) {
+      await this.extendForeignObjectAsync(this.groupsDir, {
+        common: {
+          default: JSON.stringify([]),
+          schema: this.groupsConfig
+        }
+      });
+    }
+
+    // Subscribe to device configuration changes
+    this.subscribeForeignObjects(this.devicesDir);
+
+    // Create device control states
+    for (const device of this.devices) {
+      const deviceDir = `${this.namespace}.BluOS.${device.name}`;
+      const playDataPoint = `${deviceDir}.Play`;
+      const skipDataPoint = `${deviceDir}.Skip`;
+      const backDataPoint = `${deviceDir}.Back`;
+      const pauseDataPoint = `${deviceDir}.Pause`;
+      const volumeDataPoint = `${deviceDir}.Volume`;
+      const statusDataPoint = `${deviceDir}.Status`;
+
+      this.setObjectNotExists(playDataPoint, {
+        type: 'state',
+        common: {
+          name: 'Play',
+          role: 'button',
+          type: 'boolean',
+          read: false,
+          write: true,
+          def: false,
+          desc: 'Play'
+        },
+        native: {}
+      });
+
+      this.setObjectNotExists(skipDataPoint, {
+        type: 'state',
+        common: {
+          name: 'Skip',
+          role: 'button',
+          type: 'boolean',
+          read: false,
+          write: true,
+          def: false,
+          desc: 'Skip'
+        },
+        native: {}
+      });
+
+      this.setObjectNotExists(backDataPoint, {
+        type: 'state',
+        common: {
+          name: 'Back',
+          role: 'button',
+          type: 'boolean',
+          read: false,
+          write: true,
+          def: false,
+          desc: 'Back'
+        },
+        native: {}
+      });
+
+      this.setObjectNotExists(pauseDataPoint, {
+        type: 'state',
+        common: {
+          name: 'Pause',
+          role: 'button',
+          type: 'boolean',
+          read: false,
+          write: true,
+          def: false,
+          desc: 'Pause'
+        },
+        native: {}
+      });
+
+      this.setObjectNotExists(volumeDataPoint, {
+        type: 'state',
+        common: {
+          name: 'Volume',
+          role: 'level.volume',
+          type: 'number',
+          read: true,
+          write: true,
+          def: 50,
+          desc: 'Volume'
+        },
+        native: {}
+      });
+
+      this.setObjectNotExists(statusDataPoint, {
+        type: 'state',
+        common: {
+          name: 'Status',
+          role: 'media.status',
+          type: 'string',
+          read: true,
+          write: false,
+          desc: 'Player Status'
+        },
+        native: {}
+      });
+
+      this.subscribeStates(playDataPoint);
+      this.subscribeStates(skipDataPoint);
+      this.subscribeStates(backDataPoint);
+      this.subscribeStates(pauseDataPoint);
+      this.subscribeStates(volumeDataPoint);
+
+      this.on('stateChange', async (id, state) => {
+        if (!state || state.ack) {
+          return;
+        }
+
+        const deviceId = id.split('.').pop();
+        const deviceIp = this.devices.find(device => device.name === deviceId).ip;
+
+        if (id.endsWith('Play') && state.val === true) {
+          await this.controlPlayback(deviceIp, 'play');
+        } else if (id.endsWith('Skip') && state.val === true) {
+          await this.controlPlayback(deviceIp, 'skip');
+        } else if (id.endsWith('Back') && state.val === true) {
+          await this.controlPlayback(deviceIp, 'back');
+        } else if (id.endsWith('Pause') && state.val === true) {
+          await this.controlPlayback(deviceIp, 'pause');
+        } else if (id.endsWith('Volume')) {
+          const volume = state.val;
+          await this.setVolume(deviceIp, volume);
+        }
+      });
+
+      this.startPlayerStatusPolling(device.name);
+    }
+  }
+
+  async controlPlayback(deviceIp, command) {
+    const url = `http://${deviceIp}:11000/Play`;
+    const params = { command };
+    await axios.post(url, params);
+  }
+
+  async setVolume(deviceIp, volume) {
+    const url = `http://${deviceIp}:11000/Volume`;
+    const params = { volume };
+    await axios.post(url, params);
+  }
+
+  async getPlayerStatus(deviceName) {
+    const device = this.devices.find(device => device.name === deviceName);
+    if (!device) {
+      return;
+    }
+
+    const url = `http://${device.ip}:11000/Status`;
+    const response = await axios.get(url);
+    const statusDataPoint = `${this.namespace}.BluOS.${device.name}.Status`;
+    this.setStateAsync(statusDataPoint, JSON.stringify(response.data));
+  }
+
+  startPlayerStatusPolling(deviceName) {
+    setInterval(() => {
+      this.getPlayerStatus(deviceName);
+    }, 1000);
+  }
+
+  async onForeignObjectChange(id, obj) {
+    if (id === this.devicesDir) {
+      if (obj && obj.common && obj.common.default) {
+        this.devices = JSON.parse(obj.common.default);
+      } else {
+        this.devices = [];
+      }
+    }
+  }
+}
+
+if (module.parent) {
+  module.exports = (options) => new BluOSAdapter(options);
+} else {
+  (() => new BluOSAdapter())();
+}
 	/**
 	 * Is called when adapter shuts down - callback has to be called under any circumstances!
 	 * @param {() => void} callback
